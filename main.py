d81f4f8a242737c4cf7d4f7f96f7d7b7966b1199
fukushimahayato <hayato@mylops.jp>
2023-03-21 22:21:12 +0900

    sessionのcommitなどに関するfunctionは切り分けた

--- main.py
+++ main.py
@@ -15,13 +15,14 @@ logging.basicConfig(level=logging.INFO, stream=sys.stdout )
 if __name__ == "__main__":
     import app.models
 
-    now1 = datetime.datetime(2021,1,2,3,4,5)
-    # BtcBusdBaseCandle1M.create(now1, 1.1,2.2,3.3,4.4,5)
-    candle = BtcBusdBaseCandle1M.get(now1)
+    now2 = datetime.datetime(2022,1,2,3,4,5)
+    # BtcBusdBaseCandle1M.create(now2, 1.1,2.2,3.3,4.4,5)
+    candle = BtcBusdBaseCandle1M.get(now2)
     print(candle.time)
     print(candle.open)
-    candle.open = 10000.1
+    candle.open = 100.01
     candle.save()
  
-    updated_candle = BtcBusdBaseCandle1M.get(now1)
-    print(updated_candle.open)
+    update_candle = BtcBusdBaseCandle1M.get(now2)
+    print(update_candle.time)
+    print(update_candle.open)

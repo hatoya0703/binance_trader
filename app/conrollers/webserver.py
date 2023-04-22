eaef2b13585f7d7b84ae80f51f9ea829139a493a
fukushimahayato <hayato@mylops.jp>
2023-04-22 22:17:49 +0900

    ajaxでデータ取得

--- app/conrollers/webserver.py
+++ app/conrollers/webserver.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import jsonify
 
 import settings
+import constans
 from app.models.dfcandle import DataFrameCandle
 
 app = Flask(__name__, template_folder='../views')
@@ -13,11 +16,35 @@ def remove_session(ex=None):
 
 @app.route('/')
 def index():
-    df = DataFrameCandle(settings.symbol, settings.trade_duration)
-    df.set_all_candles(settings.past_period)
+    return render_template('./chart.html')
+
+@app.route('/api/candle/', methods=['GET'])
+def api_make_handler():
     
-    candles = df.candles
-    return render_template('./google.html', candles=candles) 
+    symbole = request.args.get('symbol')
+    if not symbole:
+        return jsonify({'error': 'no symbole params'}), 400
+
+    limit_str = request.args.get('limit')
+    limit = 1000
+    if limit_str:
+        limit = int(limit_str)
+
+    if limit < 0 or limit > 1000:
+        limit = 1000
+
+    duration = request.args.get('duration')
+    if not duration:
+        duration = constans.DURATION_5M
+
+    df = DataFrameCandle(symbole, duration)
+    df.set_all_candles(limit)
+    return jsonify(df.value), 200 
+    # df = DataFrameCandle(settings.symbol, settings.trade_duration)
+    # df.set_all_candles(settings.past_period)
+    
+    # candles = df.candles
+    # return render_template('./google.html', candles=candles) 
 
 
 def start():

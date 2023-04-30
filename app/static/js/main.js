a840d00572d1df40289cf553e5e1b7cdbecef4e8
hatoya0703 <hatoya0703@gmail.com>
2023-04-30 19:15:52 +0900

    smaの線グラフ描画機能を追加

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -11,7 +11,23 @@ var config = {
         limit: 365,
         numViews: 5,
     },
+    dataTable: {
+        index : 0,
+        value: null
+    },
+    sma: {
+        enable: false,
+        indexes: [],
+        periods: [],
+        values: []
+    },
 };
+
+function initConfigValues(){
+    config.dataTable.index = 0;
+    config.sma.indexes = [];
+    config.sma.values = [];
+}
 
 function drawChart(dataTable) {
     var chartDiv = document.getElementById('chart_div');
@@ -49,6 +65,13 @@ function drawChart(dataTable) {
     var options = mainChart.getOptions();
     var view = mainChart.getView();
 
+    if (config.sma.enable == true) {
+        for (i = 0; i < config.sma.indexes.length; i++) {
+            options.series[config.sma.indexes[i]] = {type: 'line'};
+            view.columns.push(config.candlestick.numViews + config.sma.indexes[i]);
+        }
+    }
+
     var controlWrapper = new google.visualization.ControlWrapper({
         'controlType': 'ChartRangeFilter',
         'containerId': 'filter_div',
@@ -76,8 +99,17 @@ function send () {
         symbol: config.candlestick.symbol,
         limit: config.candlestick.limit,
         duration: config.candlestick.duration,
+    };
+
+    if (config.sma.enable == true) {
+        params["sma"] = true;
+        params["smaPeriod1"] = config.sma.periods[0];
+        params["smaPeriod2"] = config.sma.periods[1];
+        params["smaPeriod3"] = config.sma.periods[2];
     }
+
     $.get("/api/candle/", params).done(function (data) {
+        initConfigValues();
         var dataTable = new google.visualization.DataTable();
         dataTable.addColumn('date', 'Date');
         dataTable.addColumn('number', 'Low');
@@ -86,14 +118,34 @@ function send () {
         dataTable.addColumn('number', 'High');
         dataTable.addColumn('number', 'Volume');
 
+        if (data["smas"] != undefined) {
+            for (i = 0; i < data['smas'].length; i++){
+                var smaData = data['smas'][i];
+                if (smaData.length == 0){ continue; }
+                config.dataTable.index += 1;
+                config.sma.indexes[i] = config.dataTable.index;
+                dataTable.addColumn('number', 'SMA' + smaData["period"].toString());
+                config.sma.values[i] = smaData["values"]
+            }
+        }
+
         var googleChartData = [];
         var candles = data["candles"];
 
         for(var i=0; i < candles.length; i++){
             var candle = candles[i];
             var date = new Date(candle.time);
-            var datas = [date, candle.low, candle.open,
-                candle.close, candle.high, candle.volume];
+            var datas = [date, candle.low, candle.open, candle.close, candle.high, candle.volume];
+
+            if (data["smas"] != undefined) {
+                for (j = 0; j < config.sma.values.length; j++) {
+                    if (config.sma.values[j][i] == 0) {
+                        datas.push(null);
+                    } else {
+                        datas.push(config.sma.values[j][i]);
+                    }
+                }
+            }
 
             googleChartData.push(datas)
         }
@@ -108,13 +160,35 @@ function changeDuration(s){
     send();
 }
 
-setInterval(send, 1000 * 3)
+setInterval(send, 1000 * 3);
 window.onload = function () {
-    send()
+    send();
 
     $('#dashboard_div').mouseenter(function() {
         config.api.enable = false;
     }).mouseleave(function() {
         config.api.enable = true;
+    });
+
+    $('#inputSma').change(function() {
+        if (this.checked === true) {
+            config.sma.enable = true;
+        } else {
+            config.sma.enable = false;
+        }
+        send();
+    });
+
+    $("#inputSmaPeriod1").change(function() {
+        config.sma.periods[0] = this.value;
+        send();
+    });
+    $("#inputSmaPeriod2").change(function() {
+        config.sma.periods[1] = this.value;
+        send();
+    });
+    $("#inputSmaPeriod3").change(function() {
+        config.sma.periods[2] = this.value;
+        send();
     });
 }

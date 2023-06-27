d56e42f69716eab0c1b5d0d694e279f35fc4c6f2
hatoya0703 <hatoya0703@gmail.com>
2023-06-27 19:16:08 +0900

    一目均衡表のファンクション用ファイルを作成

--- trading_algo/algo.py
+++ trading_algo/algo.py
@@ -1,17 +1,17 @@
 import numpy as np
+
+
 def min_max(in_real):
     min_val = in_real[0]
     max_val = in_real[0]
-
     for price in in_real:
-        if price < min_val:
+        if min_val > price:
             min_val = price
-        if price > max_val:
+        if max_val < price:
             max_val = price
-
     return min_val, max_val
 
-# 一目均衡表を求める処理
+
 def ichimoku_cloud(in_real):
     length = len(in_real)
     tenkan = [0] * min(9, length)
@@ -19,24 +19,20 @@ def ichimoku_cloud(in_real):
     senkou_a = [0] * min(26, length)
     senkou_b = [0] * min(52, length)
     chikou = [0] * min(26, length)
-
     for i in range(len(in_real)):
-        if i >= 9: # 9日間のデータが溜まったら転換線を求める
-            min_val, max_val = min_max(in_real[i-9:i]) # 9日間の最小値と最大値を取得
-            tenkan.append((min_val + max_val) / 2) # 9日間の最小値と最大値の平均値を転換線とする
-        if i >= 26: # 26日間のデータが溜まったら基準線を求める
-            min_val, max_val = min_max(in_real[i-26:i]) # 26日間の最小値と最大値を取得
-            kijun.append((min_val + max_val) / 2) # 26日間の最小値と最大値の平均値を基準線とする
+        if i >= 9:
+            min_val, max_val = min_max(in_real[i-9:i])
+            tenkan.append((min_val + max_val) / 2)
+        if i >= 26:
+            min_val, max_val = min_max(in_real[i-26:i])
+            kijun.append((min_val + max_val) / 2)
+            senkou_a.append((tenkan[i] + kijun[i]) / 2)
+            chikou.append(in_real[i-26])
+        if i >= 52:
+            min_val, max_val = min_max(in_real[i-52:i])
+            senkou_b.append((min_val + max_val) / 2)
 
-            senkou_a.append((tenkan[i] + kijun[i]) / 2) # 転換線と基準線の平均値を先行スパンAとする
-
-            chikou.append(in_real[i-26]) # 26日前の終値を遅行スパンとする
-
-        if i >= 52: # 52日間のデータが溜まったら先行スパンBを求める
-            min_val, max_val = min_max(in_real[i-52:i]) # 52日間の最小値と最大値を取得
-            senkou_b.append((min_val + max_val) / 2) # 52日間の最小値と最大値の平均値を先行スパンBとする
-
-        senkou_a = ([0] * 26) + senkou_a[:length-26] # 先行スパンAを26日分ずらす
-        senkou_b = ([0] * 26) + senkou_b[:length-26] # 先行スパンBを26日分ずらす
-
+    senkou_a = ([0] * 26) + senkou_a[:-26]
+    senkou_b = ([0] * 26) + senkou_b[:-26]
     return tenkan, kijun, senkou_a, senkou_b, chikou
+
